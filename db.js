0b4451e4736a8f3e99298a79cd4df4fc07a749f6
HabibaBassel34 <habibabasse.04@gmail.com>
2025-12-25 07:03:44 +0200

    backend Phase 1

--- db.js
+++ db.js
@@ -8,7 +8,10 @@ const db = mysql.createConnection({
 });
 
 db.connect(err => {
-    if (err) throw err;
+    if (err) {
+        console.error("Database connection failed:", err);
+        return;
+    }
     console.log("MySQL connected (db.js)");
 });
 
