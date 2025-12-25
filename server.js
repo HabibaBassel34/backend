534a69004bc1d65b2ca39de5c64318e418403391
HabibaBassel34 <habibabasse.04@gmail.com>
2025-12-25 19:33:05 +0200

    Fix: use dynamic PORT for OpenShift

--- server.js
+++ server.js
@@ -22,4 +22,5 @@ app.use("/tasks", require("./routes/tasks"));
 app.use("/search", require("./routes/search"));
 app.use("/invitations", require("./routes/invitations"));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
